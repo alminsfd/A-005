f6c6e9c1fc51beea6c21bb299ab5fc4094acf9cd
alminsfd <alaminhossaintanvir42@gmail.com>
2025-08-28 13:23:47 +0600

    Calling Alert functionalities complete

--- app.js
+++ app.js
@@ -7,3 +7,14 @@ for(let heart of hearts){
     document.getElementById("heart-count").innerText=heartCount;
 })
 }
+// call btn functionality
+let callBtn=document.getElementsByClassName("call-btn")
+for(let btn of callBtn){
+    btn.addEventListener("click",function(){
+        let card=this.parentElement;
+        let cardName=card.children[1].innerText;
+        let phoneNumber=card.children[3].innerText;
+        alert(`Calling ${cardName} on ${phoneNumber}`)
+    })
+}
+
