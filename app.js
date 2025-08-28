71699b3044975ee2c327925c9d3c512489824542
alminsfd <alaminhossaintanvir42@gmail.com>
2025-08-28 15:24:02 +0600

    Coin reduce functionalities complete

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 //heart increasing
-let hearts=document.getElementsByClassName("child")
-let heartCount=0;
-for(let heart of hearts){
-    heart.addEventListener("click",function(){
-    heartCount=heartCount+1
-    document.getElementById("heart-count").innerText=heartCount;
-})
-}
+// let hearts=document.getElementsByClassName("child")
+// let heartCount=0;
+// for(let heart of hearts){
+//     heart.addEventListener("click",function(){
+//     heartCount=heartCount+1
+//     document.getElementById("heart-count").innerText=heartCount;
+// })
+// }
 // call btn functionality
+
+
 let callBtn=document.getElementsByClassName("call-btn")
 for(let btn of callBtn){
     btn.addEventListener("click",function(){
-        let card=this.parentElement;
-        let cardName=card.children[1].innerText;
-        let phoneNumber=card.children[3].innerText;
+        let coin=document.getElementById("coin").innerText
+        let coinCount=parseInt(coin)
+        if(coinCount>20){
+            coinCount=coinCount-20
+            document.getElementById("coin").innerText=coinCount
+            let card=this.parentElement;
+            let cardName=card.children[1].innerText;
+            let phoneNumber=card.children[3].innerText;
         alert(`Calling ${cardName} on ${phoneNumber}`)
+        }else{
+            alert("You do not have enough balance, please recharge.")
+        }
+
+
     })
+
 }
 
