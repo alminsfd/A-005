812c55a7e375e2e9c6060eefd3469e077fa228c9
alminsfd <alaminhossaintanvir42@gmail.com>
2025-08-28 19:05:14 +0600

    add call history  functionalities complete

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
 //heart increasing
-// let hearts=document.getElementsByClassName("child")
-// let heartCount=0;
-// for(let heart of hearts){
-//     heart.addEventListener("click",function(){
-//     heartCount=heartCount+1
-//     document.getElementById("heart-count").innerText=heartCount;
-// })
-// }
+let hearts=document.getElementsByClassName("heart-btn")
+let heartCount=0;
+for(let heart of hearts){
+    heart.addEventListener("click",function(){
+    heartCount=heartCount+1
+    document.getElementById("heart-count").innerText=heartCount;
+})
+}
 // call btn functionality
-
-
+let history=document.querySelector(".history")
 let callBtn=document.getElementsByClassName("call-btn")
 for(let btn of callBtn){
     btn.addEventListener("click",function(){
         let coin=document.getElementById("coin").innerText
         let coinCount=parseInt(coin)
-        if(coinCount>20){
+        if(coinCount>0){
             coinCount=coinCount-20
             document.getElementById("coin").innerText=coinCount
             let card=this.parentElement;
             let cardName=card.children[1].innerText;
             let phoneNumber=card.children[3].innerText;
-        alert(`Calling ${cardName} on ${phoneNumber}`)
+            alert(`Calling ${cardName} on ${phoneNumber}...`)
+            const data={
+                serviceName:cardName,
+                number:phoneNumber,
+                date:new Date().toLocaleTimeString()
+            }
+
+            let div=document.createElement("div")
+            div.innerHTML=`
+             <div class="flex md:flex-row flex-col justify-between bg-white rounded-xl  p-5 shadow-xs">
+                    <div class="dyanamics">
+                        <h2 class="text-[#111111] text-xl hind-madurai-bold ">${data.serviceName}</h2>
+                        <p class="hind-madurai-regular text-lg text-[#5C5C5C]">${data.number}</p>
+                    </div>
+                    <h3 class="hind-madurai-regular text-lg text-[#111111]">${data.date}</h3>
+                </div>            
+            
+            `
+            history.appendChild(div)
+        
+
+            
         }else{
-            alert("You do not have enough balance, please recharge.")
+            alert("You don't have enough coin, At lest 20 coin is need for call.")
         }
-
-
     })
 
 }
